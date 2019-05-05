ed7501a550c76ab0525d7379eb1557436a509401
afeiyyt <afeiyyt@foxmail.com>
2019-05-05 14:05:31 +0800

    commit Primary

---
+++ Primary/C1/ex1.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+content = requests.get('https://www.qiushibaike.com/text/').content
+soup = BeautifulSoup(content, 'html.parser')
+for div in soup.find_all('div', {'class':'content'}):
+    print div.text.strip()
